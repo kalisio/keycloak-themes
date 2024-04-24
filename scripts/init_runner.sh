1e722c39f7836516018560327b81bb03eb81bf87
cnouguier <ch.nouguier@gmail.com>
2024-04-24 15:25:44 +0200

    wip: Refactor CI to use GitHub actions and generic bash scripts #7

---
+++ scripts/init_runner.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+# set -x
+
+JOB_ID=$1
+
+THIS_FILE=$(readlink -f "${BASH_SOURCE[0]}")
+THIS_DIR=$(dirname "$THIS_FILE")
+
+. "$THIS_DIR/kash/kash.sh"
+
+### Github Actions
+
+init_github_build() {
+    install_reqs age sops
+}
+
+begin_group "Init $CI_ID for $JOB_ID"
+
+init_"${CI_ID}_${JOB_ID}"
+
+end_group "Init $CI_ID for $JOB_ID"
